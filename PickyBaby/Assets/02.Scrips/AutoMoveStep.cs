b8064b73e8ebe213748db7008f1f65ed1f18e422
Marey <kamin_s2@g.hongik.ac.kr>
2021-12-01 23:06:37 +0900

    Map2:rotating_step
    구현완료.
    ✅ player가 발판에 올라갔을때, 발판의 부모오브젝트로 들어가는 로직때문에 같이 회전함. 이 문제 해결해주기.

--- PickyBaby/Assets/02.Scrips/AutoMoveStep.cs
+++ PickyBaby/Assets/02.Scrips/AutoMoveStep.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AutoMoveStep : MonoBehaviour
 {
+    public bool rotating = false;
+    public float speed = 0.01f;
     [SerializeField]
     private GameObject startpos;
     [SerializeField]
@@ -23,23 +25,48 @@ public class AutoMoveStep : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move();
+        if (!rotating) move();
+        else
+            rotationMove();
+
 
     }
 
-   
+    void rotationMove()
+    {
+        // The step size is equal to speed times frame time.
+        var step = speed * Time.deltaTime;
+        if (trigger == true)
+        {
 
-    void move()//수직이동
+            Quaternion targetRotation = Quaternion.LookRotation(endpos.transform.position - transform.position, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3.0f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, endpos.transform.position, 0.005f);
+
+
+        }
+        if (trigger == false)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(startpos.transform.position - transform.position, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3.0f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, startpos.transform.position, 0.005f);
+        }
+        
+
+                                                                                                                                                                                                                      
+    }
+
+    void move()//수직수평이동
     {
         if (trigger==true)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, endpos.transform.position, 0.01f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, endpos.transform.position, speed);
 
 
         }
         if(trigger==false)
         {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, startpos.transform.position, 0.01f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, startpos.transform.position, speed);
       
         }
     }
