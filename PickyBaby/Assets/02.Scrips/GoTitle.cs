eb1d2ec420c63e0bb2cff914385952f02898a481
happy-jinsu <68191793+happy-jinsu@users.noreply.github.com>
2021-12-13 02:45:10 +0900

    성공, 실패 씬 구현

---
+++ PickyBaby/Assets/02.Scrips/GoTitle.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoTitle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goTitle()
+    {
+        SceneManager.LoadScene("start");
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+
+    public void Gomain1()
+    {
+        SceneManager.LoadScene("main01");
+    }
+}
